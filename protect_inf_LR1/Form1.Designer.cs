e597318c91fafb55e87b44eda1df9bc4530d0b75
mainkravsik <mainkravsik@gmail.com>
2018-07-04 03:17:52 +0700

    Initial commit

---
+++ protect_inf_LR1/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace protect_inf_LR1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButtonVizhener = new System.Windows.Forms.RadioButton();
+            this.radioButtonChezar = new System.Windows.Forms.RadioButton();
+            this.buttonEncrypt = new System.Windows.Forms.Button();
+            this.buttonDecipher = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxKeyWord = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButtonVizhener);
+            this.groupBox1.Controls.Add(this.radioButtonChezar);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(124, 79);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Метод шифрования";
+            // 
+            // radioButtonVizhener
+            // 
+            this.radioButtonVizhener.AutoSize = true;
+            this.radioButtonVizhener.Location = new System.Drawing.Point(6, 47);
+            this.radioButtonVizhener.Name = "radioButtonVizhener";
+            this.radioButtonVizhener.Size = new System.Drawing.Size(108, 17);
+            this.radioButtonVizhener.TabIndex = 1;
+            this.radioButtonVizhener.Text = "Шифр Виженера";
+            this.radioButtonVizhener.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonChezar
+            // 
+            this.radioButtonChezar.AutoSize = true;
+            this.radioButtonChezar.Checked = true;
+            this.radioButtonChezar.Location = new System.Drawing.Point(6, 24);
+            this.radioButtonChezar.Name = "radioButtonChezar";
+            this.radioButtonChezar.Size = new System.Drawing.Size(95, 17);
+            this.radioButtonChezar.TabIndex = 0;
+            this.radioButtonChezar.TabStop = true;
+            this.radioButtonChezar.Text = "Шифр Цезаря";
+            this.radioButtonChezar.UseVisualStyleBackColor = true;
+            // 
+            // buttonEncrypt
+            // 
+            this.buttonEncrypt.Location = new System.Drawing.Point(12, 307);
+            this.buttonEncrypt.Name = "buttonEncrypt";
+            this.buttonEncrypt.Size = new System.Drawing.Size(95, 23);
+            this.buttonEncrypt.TabIndex = 1;
+            this.buttonEncrypt.Text = "Зашифровать";
+            this.buttonEncrypt.UseVisualStyleBackColor = true;
+            this.buttonEncrypt.Click += new System.EventHandler(this.buttonEncrypt_Click);
+            // 
+            // buttonDecipher
+            // 
+            this.buttonDecipher.Location = new System.Drawing.Point(287, 307);
+            this.buttonDecipher.Name = "buttonDecipher";
+            this.buttonDecipher.Size = new System.Drawing.Size(112, 23);
+            this.buttonDecipher.TabIndex = 2;
+            this.buttonDecipher.Text = "Расшифровать";
+            this.buttonDecipher.UseVisualStyleBackColor = true;
+            this.buttonDecipher.Click += new System.EventHandler(this.buttonDecipher_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(168, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ключ:";
+            // 
+            // textBoxKeyWord
+            // 
+            this.textBoxKeyWord.Location = new System.Drawing.Point(171, 33);
+            this.textBoxKeyWord.Name = "textBoxKeyWord";
+            this.textBoxKeyWord.Size = new System.Drawing.Size(163, 20);
+            this.textBoxKeyWord.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 224);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(387, 77);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 119);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(387, 77);
+            this.textBox2.TabIndex = 6;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Исходный текст";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 208);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(123, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Зашифрованный текст";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(427, 342);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBoxKeyWord);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonDecipher);
+            this.Controls.Add(this.buttonEncrypt);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Шифрофка";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButtonVizhener;
+        private System.Windows.Forms.RadioButton radioButtonChezar;
+        private System.Windows.Forms.Button buttonEncrypt;
+        private System.Windows.Forms.Button buttonDecipher;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxKeyWord;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ protect_inf_LR1/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace protect_inf_LR1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            N = characters.Length;
+        }
+        char[] characters = new char[] { 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и',
+                                                        'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с',
+                                                        'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ь', 'ы', 'ъ',
+                                                        'э', 'ю', 'я', 'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И',
+                                                        'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 
+                                                        'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ь', 'Ы', 'Ъ',
+                                                        'Э', 'Ю', 'Я', ' ', '.', ',', '?', '!', '1', '2', '3', '4', '5', '6', '7',
+                                                        '8', '9', '0', 'A', 'E', 'I', 'O', 'U', 'Y', 'B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z',
+                                        'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'z', 'x', 'c', 'v', 'b', 'n', 'm'};
+
+        private int N; //длина алфавита
+
+        //Основной цикл шифровки
+        private void buttonEncrypt_Click(object sender, EventArgs e)
+        {
+            if (radioButtonChezar.Checked)
+            {
+                
+                int key = 1;
+                string s = textBox2.Text;
+                key = Convert.ToInt32(textBoxKeyWord.Text);
+                textBox1.Text = Encodeche(s,key);
+            }
+            if (radioButtonVizhener.Checked)
+            {
+                string s = textBox2.Text;
+                textBox1.Text = Encode(s, Convert.ToString(textBoxKeyWord.Text));
+            }
+
+        }
+
+        //Основной цикл расшифровки
+        private void buttonDecipher_Click(object sender, EventArgs e)
+        {
+            if (radioButtonChezar.Checked)
+            {
+                int key = 1;
+                string s = textBox1.Text;
+                key = Convert.ToInt32(textBoxKeyWord.Text);
+                textBox2.Text = Decodeche(s, key);
+            }
+            if (radioButtonVizhener.Checked)
+            {
+                if (textBoxKeyWord.Text.Length > 0)
+                {
+                    string s = textBox1.Text;
+                    textBox2.Text = Decode(s, Convert.ToString(textBoxKeyWord.Text));
+                }
+                else
+                    MessageBox.Show("Введите ключевое слово!");
+            }
+        }
+
+        //Метод шифрования Виженера
+        private string Encode(string input, string keyword)
+        {
+            
+            string result = "";
+
+            int keyword_index = 0;
+
+            foreach (char symbol in input)
+            {
+                int c = (Array.IndexOf(characters, symbol) +
+                    Array.IndexOf(characters, keyword[keyword_index])) % N;
+
+                result += characters[c];
+
+                keyword_index++;
+
+                if ((keyword_index + 1) == keyword.Length)
+                    keyword_index = 0;
+            }
+
+            return result;
+        }
+
+        //Метод расшифрования Виженера
+        private string Decode(string input, string keyword)
+        {
+            
+            string result = "";
+
+            int keyword_index = 0;
+
+            foreach (char symbol in input)
+            {
+                int p = (Array.IndexOf(characters, symbol) + N -
+                    Array.IndexOf(characters, keyword[keyword_index])) % N;
+
+                result += characters[p];
+
+                keyword_index++;
+
+                if ((keyword_index + 1) == keyword.Length)
+                    keyword_index = 0;
+            }
+
+            return result;
+        }
+
+        //Метод шифрования Цезаря
+        private string Encodeche(string input, int keyword)
+        {
+            string result = "";
+
+            int p = 0;
+
+            foreach (char symbol in input)
+            {
+                for (int i = 0;i< N; i++)
+                {
+                    if(symbol==characters[i])
+                    {
+                        p = i + keyword;
+                        while (p >= N)
+                            p -= N;
+                    }
+                }
+                result += characters[p];
+            }
+
+            return result;
+        }
+
+        //Метод расшифрования Цезаря
+        private string Decodeche(string input, int keyword)
+        {
+            string result = "";
+
+            int p = 0;
+
+            foreach (char symbol in input)
+            {
+                for (int i = 0; i < N; i++)
+                {
+                    if (symbol == characters[i])
+                    {
+                        p = i - keyword;
+                        while (p < 0)
+                            p += N;
+                    }
+                }
+                result += characters[p];
+            }
+
+            return result;
+        }
+    }
+}
